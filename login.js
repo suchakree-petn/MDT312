911269d1cdbceece39efa93683ef4da7b38542c9
suchakree-petn <111336719+suchakree-petn@users.noreply.github.com>
2023-09-12 15:14:25 +0700

    ADD: Add login and register page with css

---
+++ login.js
@@ -0,0 +1,10 @@
+window.onload = loginLoad;
+function loginLoad(){
+	
+}
+
+function checkLogin(){
+	//ถ้าตรวจสอบแล้วพบว่ามีการ login ไม่ถูกต้อง ให้ return false ด้วย
+}
+
+			
---
+++ register.js
@@ -0,0 +1,8 @@
+window.onload = pageLoad;
+function pageLoad(){
+	
+}
+
+function validateForm() {
+    //ถ้าตรวจสอบแล้วว่ามีการ register ไม่ถูกต้องให้ return false ด้วย
+}
