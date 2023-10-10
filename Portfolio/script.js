3c20fe369e1c88b3ca7cf729b967fdd621acb92a
suchakree-petn <111336719+suchakree-petn@users.noreply.github.com>
2023-10-10 10:56:13 +0700

    Add files via upload

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,21 +1,18 @@
+window.onload = function() {
+    function handleSectionMouseEnter(event) {
+        const section = event.target;
+        section.style.transform = "scale(1.05)";
+        section.style.transition = "transform 0.3s";
+    }
 
-function handleSectionMouseEnter(sectionId) {
-    const section = document.getElementById(sectionId);
-    section.style.transform = "scale(1.05)";
-    section.style.transition = "transform 0.3s";
-}
-
-function handleSectionMouseLeave(sectionId) {
-    const section = document.getElementById(sectionId);
-    section.style.transform = "scale(1)";
-    section.style.transition = "transform 0.3s";
-}
-
-document.getElementById("hobbies").addEventListener("mouseenter", () => handleSectionMouseEnter("hobbies"));
-document.getElementById("hobbies").addEventListener("mouseleave", () => handleSectionMouseLeave("hobbies"));
-document.getElementById("skills").addEventListener("mouseenter", () => handleSectionMouseEnter("skills"));
-document.getElementById("skills").addEventListener("mouseleave", () => handleSectionMouseLeave("skills"));
-document.getElementById("interests").addEventListener("mouseenter", () => handleSectionMouseEnter("interests"));
-document.getElementById("interests").addEventListener("mouseleave", () => handleSectionMouseLeave("interests"));
-
-
+    function handleSectionMouseLeave(event) {
+        const section = event.target;
+        section.style.transform = "scale(1)";
+        section.style.transition = "transform 0.3s";
+    }
+    const sections = document.querySelectorAll(".section");
+    sections.forEach(function(section) {
+        section.addEventListener("mouseenter", handleSectionMouseEnter);
+        section.addEventListener("mouseleave", handleSectionMouseLeave);
+    });
+};
